e301e61887138d305d86379f3b58be3cf809b8c2
yupitel <qfoori@gmail.com>
2022-11-21 05:34:41 +0900

    update environment.cpp

--- src/environment.cpp
+++ src/environment.cpp
@@ -43,9 +43,16 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     
     // RENDER OPTIONS
     bool renderScene = true;
+    // bool renderScene = false; // in case of render point cloud
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor 
+    Lidar* lidar = new Lidar(cars, 0);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr inputCloud = lidar->scan();
+    renderRays(viewer, lidar->position, inputCloud);
+    
+    // renderPointCloud(viewer, inputCloud, "inputCloud"); // show point cloud raw data
+
 
     // TODO:: Create point processor
   
